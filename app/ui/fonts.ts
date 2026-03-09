995cf9aa8cb6d12858b801f01dc647cbeafc6cde
upne <iambz00@gmail.com>
2026-03-09 15:44:53 +0900

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Nanum_Gothic_Coding, Lusitana } from 'next/font/google';
+
+export const font_ngc = Nanum_Gothic_Coding({
+  weight: ['400', '700'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+});
